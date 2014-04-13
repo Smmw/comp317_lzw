d53dd325e1811664fefd1e4d23e4e0afa4effd23
Severin <GazerDwarf@Gmail.com>
2014-04-13 13:05:51 +1200

    Added more skeleton, will not compile as some methods do not return anything.

--- SuffixTrie.java
+++ SuffixTrie.java
@@ -6,6 +6,54 @@
  * 1181754
  */
 public class SuffixTrie<T>{
+	Node root;
+	// For steping/walking I havn't named the method yet
+	Node pos;
+	// The next avaliable index
+	int index = 0;
+	// Stops the trie from growing forever
+	int limit;
+	
+	/*
+	 * Makes a SuffixTrie with no data
+	 */
+	public SuffixTrie(int limit){
+		self.limit = limit;
+	}
+
+	/*
+	 * Adds a node to the trie at the current position
+	 */
+	public void Add(T data){
+		// Check if no root
+		// If so add node as root, set pos to root, return
+		// Check if pos has no child
+		// If so add child
+		// Else go to leftest sister of child and add
+	}
+
+	/*
+	 * Used to walk through the trie
+	 */
+	public boolean Step(T data){
+		// If pos is null throw hissy fit
+		// If pos has child data
+		// Pos is child byte
+		// Return false, did not trip
+		// Else return true, did trip
+	}
+
+	/*
+	 * Checks if a node has a child with the value data
+	 */
+	public boolean hasChild(Node n, T data){
+		// Child is n.down
+		// While child not null
+		// If child.data is data return true
+		// End
+		// Return false
+	}
+
 	/**
 	 * A node for the suffix trie
 	 * Will be root, internal, or leaf node
