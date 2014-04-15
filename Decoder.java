c7894fadc356b0155b0d158a75c6bd376c23fe9b
Severin <GazerDwarf@Gmail.com>
2014-04-15 15:36:34 +1200

    Started on Decoder

---
+++ Decoder.java
@@ -0,0 +1,22 @@
+/**
+ * Reads strings from standard in
+ * Decompresses them with LZW using bytes as symbols
+ * Outputs bytes representing the bytes of the file
+ *
+ * Severin Mahoney-Marsh 2014
+ * 1181754
+ */
+public class Decoder{
+	public static void main(String[] args){
+		// Default settings
+		// Read args
+		// Undo LZW
+		// Initialise dictionary with all bytes
+		// Read a value
+		// While not done
+		// Output data for value
+		// Read new value
+		// Add start of new value's data to end of new dictionary entry for value
+		// Go to top of loop with new value
+	}
+}
